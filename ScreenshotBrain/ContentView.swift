b59ffb56d5531b7affb941832291a1ac8deb30c7
Medha Karthi <karthimedha@gmail.com>
2026-05-03 11:07:55 -0400

    Initial Commit

---
+++ ScreenshotBrain/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScreenshotBrain
+//
+//  Created by medha karthi on 2026-05-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScreenshotBrain/ScreenshotBrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenshotBrainApp.swift
+//  ScreenshotBrain
+//
+//  Created by medha karthi on 2026-05-03.
+//
+
+import SwiftUI
+
+@main
+struct ScreenshotBrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
